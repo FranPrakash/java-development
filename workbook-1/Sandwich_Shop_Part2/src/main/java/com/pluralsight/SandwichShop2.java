0b856b1d41f0556c91b674d36ae43efc37c02ecb
Fran-Santos <francihelen.santos25@gmail.com>
2024-09-29 19:54:08 -0700

    Added Loaded option for the sandwich shop

--- workbook-1/Sandwich_Shop_Part2/src/main/java/com/pluralsight/SandwichShop2.java
+++ workbook-1/Sandwich_Shop_Part2/src/main/java/com/pluralsight/SandwichShop2.java
@@ -17,52 +17,62 @@ public class SandwichShop2 {
         byte choice = myScanner.nextByte(); // what if I use int ?
 
         // Ask user choice for sandwich loaded
-        System.out.println(" Would you like the Sandwich Loaded ? Yes or no ?");
-        //Accept user choice using myScanner object's Next method and store it in a String variable.
-        String loadedSandwich = myScanner.next(); // Declaring this variable as string
-
-        // Telling user additional costs
+        System.out.println("Would you like the Sandwich Loaded ? ");
         System.out.println("There is an additional cost for a loaded sandwich");
         System.out.println("Additional $1.00 for regular sandwich");
         System.out.println("Additional $1.75 for large sandwich");
+        System.out.println("Please enter Yes or No : ");
+        //Accept user choice using myScanner object's Next method and store it in a String variable.
+        String loadedSandwich = myScanner.next(); // Declaring this variable as string
 
 
-        // Ask user for their age to provide discounts.
+        // User provide age
         System.out.println("Discounts are available based on your age. Please provide your age: ");
+        //Scanner object to Accept user's age
+        int age = myScanner.nextInt();
+        float basePrice; //declare the base price as a float variable // Declare variable before if statement.
 
-        float basePrice = 0.0f; //declare the base price as a float variable // Declare variable before if statement.
-        float loadedPrice = 0.0f; // My code was not running, It was asking for initialization of the variable that, is why
-        //I gave it a value.
 
+        //calculate the basePrice based on user Input and loaded option
 
-        //calculate the basePrice based on user Input
+        float loadedPrice; //declare variable loadedPrice
+
         if (choice == 1) { // If user choose choice 1
             basePrice = 5.45f;
-            if (loadedSandwich == "yes") loadedPrice = 1.0f; //User has selected loaded option
         }
         else { // user has selected choice 2
             basePrice = 8.95f;
-            if (loadedSandwich == "yes") loadedPrice = 1.75f; // user has selected loaded option
         }
-        //calculate the discount based on user Age Input
-        byte discount; //declare the discount as a byte variable
-        //Scanner object to Accept user's age and store it in an int variable
-        int age = myScanner.nextInt();
 
-        if (age <=17)  discount = 10;
-        else if (age >=65) discount =20;
-        else  discount = 0; {
+        if (loadedSandwich.equals ("yes") && choice == 1) { // I was trying to do this loadedSandwich == "yes" but didn't work and intelliJ suggest the equals method.
+            loadedPrice = 1.0f;
+        } else if (loadedSandwich.equals("yes") && choice == 2) {
+            loadedPrice = 1.75f;
+        } else loadedPrice =0.0f;
+
+
+       // Calculate discount based on user age
+        float discountAmount;
+
+        if (age <=17){
+            discountAmount = (basePrice + loadedPrice ) * 0.10f;
         }
+        else if (age >=65) {
+            discountAmount = (basePrice + loadedPrice ) * 0.20f;
+        }
+        else  discountAmount = 0.0f;
 
         // Calculate final cost of the sandwich
-        float finalCost = basePrice + loadedPrice * (100 - discount)/100;
+
+        float finalCost = basePrice + loadedPrice - discountAmount;
 
         System.out.println("Base Price: $" + basePrice);
-        System.out.println("loaded Charge :" + loadedPrice);
-        System.out.println("Discount: $" + discount);
+        System.out.println("loaded Charge: $" + loadedPrice);
+        System.out.println("Discount: $" + discountAmount);
         System.out.println("Final Price : $" + finalCost);
 
         myScanner.close();
+
     }
 
 }
