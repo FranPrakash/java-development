04b0ed5a8d46822779fe492a9ac2e7dfebf7f9f4
Fran-Santos <francihelen.santos25@gmail.com>
2024-10-01 10:51:18 -0700

    Future Value Calculator Workshop

--- workbook-1/FutureValue_Calculator/src/main/java/com/pluralsight/FutureValueCalculator.java
+++ workbook-1/FutureValue_Calculator/src/main/java/com/pluralsight/FutureValueCalculator.java
@@ -17,7 +17,7 @@ public class FutureValueCalculator {
         System.out.println("Please provide the interest Rate : ");
         double interestRate = myScanner.nextDouble();
         System.out.println("Please provide the number of Years : ");
-        int numberOfYears = myScanner.nextInt();
+        double numberOfYears = myScanner.nextDouble();
 
         // Calculations. Set the compounding frequency per year, formula says  compounded daily, so n = 365
         int n = 365;
@@ -27,7 +27,7 @@ public class FutureValueCalculator {
         //Formula for future value : A = P(1+r/n)(nt) A= future value, P = Principal or deposit , R= Annual interest rate in decimal,
         // n = is the number of times that interest in compounded every year, t = time or number of years.
 
-        double futureValue = depositAmount * Math.pow((1 + interestRate / n), (n * numberOfYears));
+        double futureValue = depositAmount * Math.pow((1 + ( r / n)), (n * numberOfYears));
 
         //Total interest earned calculations
         double interestEarned = futureValue - depositAmount;
