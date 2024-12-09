501edc1f6cdf500a0b0d01423c81ce3826856cb1
Fran-Santos <francihelen.santos25@gmail.com>
2024-12-09 11:44:08 -0800

    Finished exercise 4 workbook 9

---
+++ Workbook-9/NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/HomeController.java
@@ -0,0 +1,19 @@
+package com.pluralsight.NorthwindTradersAPI;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    // this method will respond to http://localhost:8080/
+    @RequestMapping(path="/", method=RequestMethod.GET)
+    public String index(
+            //The user should be able to enter a url with a query string that includes country(http://localhost:8080?country=USA).
+            @RequestParam(defaultValue="World") String country
+    ) {
+        return "Hello " + country + "!";
+    }
+}
+
